fa17ee8c3e095330368bcf7f2ee1d813038f5a86
Andrew Sutjipto <overfitted@gmail.com>
2023-03-14 01:29:07 +0800

    Adds starting search code

--- index.py
+++ index.py
@@ -6,20 +6,18 @@ import os
 import pickle
 import shutil
 import sys
+import collections
 
 # improvements:
 # use temp directory
 # standardise index and positingmap
 
+
 def usage():
-    print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
+    print("usage: " +
+          sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
 
-def buildDocIds(in_dir, out_file):
-    inFiles = sorted([int(i) for i in os.listdir(in_dir)])
-    with open(out_file, "wb") as out_file:
-        pickle.dump(inFiles, out_file)
 
-# assumes that input files are preprocessed
 def build_index(in_dir, out_dict, out_postings):
     """
     build index from documents stored in the input directory,
@@ -32,71 +30,74 @@ def build_index(in_dir, out_dict, out_postings):
     if not os.path.exists(in_dir):
         print("ERROR: in dir not exist")
         sys.exit(2)
-    
-    buildDocIds(in_dir, "docIds.txt")
-    dictionary = {}
-    index = {}
 
-    inFiles = sorted(os.listdir(in_dir), key = lambda x: int(x))
+    inFiles = sorted(os.listdir(in_dir), key=lambda x: int(x))
 
-    # pre-process the documents, first by tokenising the sentences, 
-    # then words, then apply porter stemming and then finally writitng the result 
-    # of the procesed file into a dictery consisting of all the processed documents. 
+    # pre-process the documents, first by tokenising the sentences,
+    # then words, then apply porter stemming and then finally writitng the result
+    # of the procesed file into a dictery consisting of all the processed documents.
 
     TMP_DIR = "processed"
     stemmer = nltk.stem.PorterStemmer()
     if not os.path.exists(TMP_DIR):
         os.makedirs(TMP_DIR)
-    
+
     for file_name in inFiles:
 
         with open(os.path.join(in_dir, file_name), 'r') as f:
             contents = f.read()
 
-        contents = contents.lower() # case folding
+        contents = contents.lower()  # case folding
         sentences = nltk.tokenize.sent_tokenize(contents)
-        
-        words = [] 
+
+        words = []
         for sentence in sentences:
-            words.extend(nltk.tokenize.word_tokenize(sentence)) 
+            words.extend(nltk.tokenize.word_tokenize(sentence))
 
         stemmed_words = [stemmer.stem(word) for word in words]
         processed_words = ' '.join(stemmed_words)
 
-         # Write processed contents to tmp directory
+        # Write processed contents to tmp directory
         with open(os.path.join(TMP_DIR, file_name), 'w') as f:
             f.write(processed_words)
-    
-    in_dir = TMP_DIR # change the input directory to the processed documents folder 
-    inFiles = sorted(os.listdir(in_dir), key = lambda x: int(x))
+
+    in_dir = TMP_DIR  # change the input directory to the processed documents folder
+    inFiles = sorted(os.listdir(in_dir), key=lambda x: int(x))
+    index = collections.defaultdict(lambda: [])
+    documentLength = collections.defaultdict(lambda: 0)
 
     for inFile in inFiles:
         with open(os.path.join(in_dir, inFile), "r", encoding="utf-8") as f:
-            fileSet = set()
+            termFreq = collections.defaultdict(lambda: 0)
 
             for line in f:
                 words = line.split()
                 for word in words:
-                    fileSet.add(word)
+                    termFreq[word] += 1
+                    documentLength[int(inFile)] += 1
 
-            for word in fileSet:
-                if word not in dictionary.keys():
-                    dictionary[word] = len(dictionary.keys())
-                    index[dictionary[word]] = []
-                index[dictionary[word]].append(int(inFile))
+            for word in termFreq:
+                index[word].append([int(inFile), termFreq[word]])
 
     startIdx = 0
 
     with open(out_postings, "wb") as postingsFile:
-        for word in dictionary:
+        for word in index:
             # pointer is in the form [start index, size of pickle-serialised linkedlist in bytes]
-            dictionary[word] = [startIdx, postingsFile.write(pickle.dumps(index[dictionary[word]]))]
-            print(word, dictionary[word][0], dictionary[word][1])
-            startIdx += dictionary[word][1]
+            pointer = [startIdx, postingsFile.write(pickle.dumps(index[word]))]
+
+            # value is in the form of [document frequency, pointer]
+            documentFrequency = len(index[word])
+            index[word] = [documentFrequency, pointer]
+
+            print(word, index[word][1][0], index[word][1][1])
+            startIdx += index[word][1][1]
 
     with open(out_dict, "wb") as dictFile:
-        pickle.dump(dictionary, dictFile)
+        pickle.dump(dict(index), dictFile)
 
+    with open("docData.txt", "wb"):
+        pickle.dump(len(documentLength))
 
     # cleanup
     shutil.rmtree(TMP_DIR)
@@ -112,9 +113,6 @@ def build_index(in_dir, out_dict, out_postings):
 #     sys.setrecursionlimit(max_rec)
 
 
-
-
-
 input_directory = output_file_dictionary = output_file_postings = None
 
 try:
@@ -124,11 +122,11 @@ except getopt.GetoptError:
     sys.exit(2)
 
 for o, a in opts:
-    if o == '-i': # input directory
+    if o == '-i':  # input directory
         input_directory = a
-    elif o == '-d': # dictionary file
+    elif o == '-d':  # dictionary file
         output_file_dictionary = a
-    elif o == '-p': # postings file
+    elif o == '-p':  # postings file
         output_file_postings = a
     else:
         assert False, "unhandled option"
--- search.py
+++ search.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-import re
 import nltk
 import sys
 import getopt
+import collections
+import math
+import pickle
+
 
 def usage():
-    print("usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results")
+    print("usage: " +
+          sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results")
+
 
 def run_search(dict_file, postings_file, queries_file, results_file):
     """
@@ -15,6 +20,68 @@ def run_search(dict_file, postings_file, queries_file, results_file):
     print('running search on the queries...')
     # This is an empty method
     # Pls implement your code in below
+
+    with open(dict_file, "rb") as dict_file:
+        # <word in string form, [document frequency, [start byte address, size in bytes]]>>
+        dictionary = pickle.load(dict_file)
+
+    with open("docData.txt", "rb") as doclen_file:
+        documentLength = pickle.load(doclen_file)
+
+    # Process the queries and output
+    scores = collections.defaultdict(lambda: 0)
+    with open(queries_file, "r") as query_file, open(results_file, "w+") as out_file:
+        for queryLine in query_file:
+            queryIndex = collections.defaultdict(lambda: 0)
+
+            query = queryLine.strip().split()
+            for word in query:
+                queryIndex[word] += 1
+
+            for word in queryIndex:
+                # get query tfidf
+                queryTf = 1 + math.log(queryIndex[word], 10)
+                queryIdf = math.log(len(documentLength)/dictionary[word][0])
+                queryWeight = queryTf * queryIdf
+
+                postingList = single_word_query(
+                    word, dictionary, postings_file)
+
+                # for each document in posting list, get tf and multiply by queryWeight
+                for freqPair in postingList:
+                    docTf = 1 + math.log(freqPair[1], 10)
+                    scores[freqPair[0]] += docTf * queryWeight
+
+            # normalise
+            for docId in scores:
+                scores[docId] /= documentLength[docId]
+
+            # chuck scores into a heap and print out the top 5
+
+
+def single_word_query(word, dictionary, postings_file):
+    """
+    Given a word in string and dictoanay and a file of documents postings 
+    it returns a list with all the documents_posting found in the dictonary. 
+    Params: word: string, dictionary : a dictonray object loaded into memory, 
+            postings_file: string path to the file of postings_file 
+
+    Returns: a list object consisting of all the documents posting of the words 
+    in the dictonary. 
+    """
+    stemmer = nltk.stem.PorterStemmer()
+    word = stemmer.stem(word.lower())  # case folding and stemming
+
+    # -1 means that the word doesn't exist
+    [_, [start, sz]] = dictionary.get(word, [-1, [-1, -1]])
+
+    if start != -1:
+        with open(postings_file, "rb") as post_file:
+            post_file.seek(start)
+            return pickle.loads(post_file.read(sz))
+
+    return []
+
 
 dictionary_file = postings_file = file_of_queries = output_file_of_results = None
 
@@ -26,7 +93,7 @@ except getopt.GetoptError:
 
 for o, a in opts:
     if o == '-d':
-        dictionary_file  = a
+        dictionary_file = a
     elif o == '-p':
         postings_file = a
     elif o == '-q':
@@ -36,7 +103,7 @@ for o, a in opts:
     else:
         assert False, "unhandled option"
 
-if dictionary_file == None or postings_file == None or file_of_queries == None or file_of_output == None :
+if dictionary_file == None or postings_file == None or file_of_queries == None or file_of_output == None:
     usage()
     sys.exit(2)
 
